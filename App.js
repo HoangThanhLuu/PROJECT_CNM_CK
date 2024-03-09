ed1bc3d745050c710000b34c8c3962fd350ab69a
HoangThanhLuu <luuhoang06102002@gmail.com>
2024-03-10 01:14:09 +0700

    Setup project

--- App.js
+++ App.js
@@ -1,16 +1,22 @@
-import {View, Text} from 'react-native';
+// eslint-disable-next-line prettier/prettier
+import { View, Text } from 'react-native';
 import React from 'react';
 import SettingScreen from './src/screens/setting';
-import {createNativeStackNavigator} from '@react-navigation/native-stack';
-import {NavigationContainer} from '@react-navigation/native';
+// eslint-disable-next-line prettier/prettier
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+// eslint-disable-next-line prettier/prettier
+import { NavigationContainer } from '@react-navigation/native';
 import AccountAndSercurityScreen from './src/screens/account_and_sercurity';
 import BottomTabs from './src/components/bottomTab';
 const Stack = createNativeStackNavigator();
 const App = () => {
   return (
     <NavigationContainer>
-      <Stack.Navigator initialRouteName="BottomTab" screenOptions={{headerShown:false}}>
-        <Stack.Screen name='BottomTab' component={BottomTabs} />
+      <Stack.Navigator
+        initialRouteName="BottomTab"
+        // eslint-disable-next-line prettier/prettier
+        screenOptions={{ headerShown: false }}>
+        <Stack.Screen name="BottomTab" component={BottomTabs} />
         <Stack.Screen name="Setting" component={SettingScreen} />
         <Stack.Screen
           name="AccountAndSercurity"
--- src/components/bottomTab.js
+++ src/components/bottomTab.js
@@ -1,35 +1,50 @@
-import { View, Text } from 'react-native'
-import React from 'react'
-import {createBottomTabNavigator} from '@react-navigation/bottom-tabs'
+/* eslint-disable prettier/prettier */
+import React from 'react';
+// eslint-disable-next-line prettier/prettier
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import { Message, User, UserTag } from 'iconsax-react-native';
 import colors from '../styles/colors';
-import MessageScreen from '../screens/message'
-import PhoneBookScreen from '../screens/phonebook'
-import AccountScreen from '../screens/account'
+import MessageScreen from '../screens/message';
+import PhoneBookScreen from '../screens/phonebook';
+import AccountScreen from '../screens/account';
 const Tab = createBottomTabNavigator();
 const BottomTabs = () => {
   return (
-    <Tab.Navigator initialRouteName='Tin nhắn' screenOptions={{headerShown:false}}>
-      <Tab.Screen name='Tin nhắn' component={MessageScreen} options={{
+    <Tab.Navigator
+      initialRouteName="Tin nhắn"
+      screenOptions={{ headerShown: false }}>
+      <Tab.Screen
+        name="Tin nhắn"
+        component={MessageScreen}
+        options={{
           tabBarIcon: ({ focused }) => (
-            <Message color={focused ? colors.header : "#000"} />
+            <Message color={focused ? colors.header : '#000'} />
           ),
           tabBarShowLabel: false,
-        }} />
-        <Tab.Screen name='Danh bạ' component={PhoneBookScreen} options={{
+        }}
+      />
+      <Tab.Screen
+        name="Danh bạ"
+        component={PhoneBookScreen}
+        options={{
           tabBarIcon: ({ focused }) => (
-            <UserTag color={focused ? colors.header : "#000"} />
+            <UserTag color={focused ? colors.header : '#000'} />
           ),
           tabBarShowLabel: false,
-        }} />
-        <Tab.Screen name='Cá nhân' component={AccountScreen} options={{
+        }}
+      />
+      <Tab.Screen
+        name="Cá nhân"
+        component={AccountScreen}
+        options={{
           tabBarIcon: ({ focused }) => (
-            <User color={focused ? colors.header : "#000"} />
+            <User color={focused ? colors.header : '#000'} />
           ),
           tabBarShowLabel: false,
-        }} />
+        }}
+      />
     </Tab.Navigator>
-  )
-}
+  );
+};
 
-export default BottomTabs
+export default BottomTabs;
